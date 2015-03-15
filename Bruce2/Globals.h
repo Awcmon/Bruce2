36774cc272030e9eb2f9a6cc01585865858e58ef
Awcmon <adcarbine@yahoo.com>
2015-03-15 02:13:03 -0700

    RIP

--- Bruce2/Globals.h
+++
@@ -1,9 +0,0 @@
-
-#include <SDL.h>
-
-#define FRAMES_PER_SECOND 60
-#define MS_PER_FRAME 17
-//#define MS_PER_FRAME 1000/FRAMES_PER_SECOND
-
-extern int SCREEN_WIDTH;
-extern int SCREEN_HEIGHT;
--- README.md
+++
@@ -1,6 +0,0 @@
-# Bruce2
-Basic ReUsable CodE 2
-
-For games and stuff.
-
-Only on Github since versioning I guess.
